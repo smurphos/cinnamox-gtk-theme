d00838e870ff655b8368bb59674d965e1e41c4f9
steve <29017677+smurphos@users.noreply.github.com>
2020-05-19 08:08:19 +0100

    Scripts - more shellcheck and indentation fixes

--- cinnamox_specific/cinnamox_toggle_GTK2_HIDPI.sh
+++ cinnamox_specific/cinnamox_toggle_GTK2_HIDPI.sh
@@ -2,26 +2,24 @@
 #Description: Helper script to toggle GTK2 HIDPI Support
 THEMENAME=#THEMENAME;
 DIRECTORY="/home/$USER/.themes/$THEMENAME/gtk-2.0/";
-cd "$DIRECTORY";
+cd "$DIRECTORY" || exit;
 if [ ! -f "$PWD/gtkrc" ]; then
-	echo "Something is wrong. Cannot find $PWD/gtkrc"
+    echo "Something is wrong. Cannot find $PWD/gtkrc"
     echo "";
-    read -p "Press enter to exit the script.";
-    cd;
+    read -rp "Press enter to exit the script.";
     exit 1;
 fi
 if [ ! -f "gtkrc.nohidpi" ]; then
     cp gtkrc gtkrc.nohidpi && cp -f gtkrc.hidpi gtkrc && echo "Enabled HIDPI in GTK2. Reloading $THEMENAME.";
     gsettings reset org.cinnamon.desktop.interface gtk-theme;
-	gsettings set org.cinnamon.desktop.interface gtk-theme "$THEMENAME";
+    gsettings set org.cinnamon.desktop.interface gtk-theme "$THEMENAME";
 else
-	cp -f gtkrc.nohidpi gtkrc && rm gtkrc.nohidpi && echo "Disabled HIDPI in GTK2. Reloading $THEMENAME.";
-	gsettings reset org.cinnamon.desktop.interface gtk-theme;
-	gsettings set org.cinnamon.desktop.interface gtk-theme "$THEMENAME";
+    cp -f gtkrc.nohidpi gtkrc && rm gtkrc.nohidpi && echo "Disabled HIDPI in GTK2. Reloading $THEMENAME.";
+    gsettings reset org.cinnamon.desktop.interface gtk-theme;
+    gsettings set org.cinnamon.desktop.interface gtk-theme "$THEMENAME";
 fi
 if [ -t 1 ]; then
     echo "";
-    read -p "Press enter to exit the script.";
+    read -rp "Press enter to exit the script.";
 fi
-cd;
 exit;
